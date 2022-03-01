1275e50b4f7cd7a7f9ac45c38a6d7bd12d9e5b51
tamer.mansour@axsos.me <tamer.mansour@axsos.me>
2022-03-01 13:03:21 +0200

    add-appjs

--- academy-app/src/App.js
+++ academy-app/src/App.js
@@ -9,16 +9,10 @@ function App() {
   return (
     <div className="App">
       <Header/>
-<<<<<<< HEAD
       {/* <Signin/> */}
       <Footer />
       {/* <Student /> */}
       {/* <StudentList/> */}
-=======
-      <Signin/>
-      <Footer /> 
-      <StudentList/>
->>>>>>> ab11e79b241dbf152dcf902caf08f6119caf0883
     </div>
   );
 }
